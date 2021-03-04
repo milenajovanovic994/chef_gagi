112f558809f15ba2ee487e81c942f7bfe8d4331a
milenajovanovic994 <milenche.jovanovic@gmail.com>
2021-03-04 10:29:08 +0100

    server bugs

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://milenajovanovic994-chef-gagi-s.herokuapp.com/'
+export const BASE_URL = 'https://milenajovanovic994-chef-gagi-s.herokuapp.com'
 
 export const RECIPES = '/recipes'
 export const USERS = '/users'
