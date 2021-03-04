73a7779b4ce6b71d4b3bca726a7467b658aac2da
milenajovanovic994 <milenche.jovanovic@gmail.com>
2021-03-04 03:44:22 +0100

    m

--- src/components/StyledGagi.jsx
+++ src/components/StyledGagi.jsx
@@ -4,7 +4,7 @@ const StyledGagi = styled.h1`
     font-family: 'Poppins', sans-serif;
     font-weight: 600;
     color: #444;
-    font-size: 30px;
+    font-size: 25px;
 `
 
 export default StyledGagi
