a0c2705675a5590e3a11ff491fbeb4ac96ebc420
milenajovanovic994 <milenche.jovanovic@gmail.com>
2021-03-04 03:40:41 +0100

    message

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
 	const [userRecipes, setUserRecipes] = useState([])
 
 	return (
-		<Router basename={'/'}>
+		<Router basename={'/https://milenajovanovic994.github.io/'}>
 			<StyledNav>
 				<StyledGagi>Chef Gagi</StyledGagi>
 				<StyledLink to="/">Home</StyledLink>
